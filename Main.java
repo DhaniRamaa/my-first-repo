7e2b68b9e500077cac7836c394081091c69254b7
Rama <ramadhaniisya@gmail.com>
2022-04-04 15:25:57 +0700

    Task 1

---
+++ Main.java
@@ -0,0 +1,17 @@
+package study;
+
+
+public class Main2 {
+    public static void main(String[] args) {
+        Biodata b= new Biodata();
+        
+        b.getFullName("Ramadhani Isya");
+        b.getPlaceBirth("Jakarta");
+        b.getDateBirth(17," November ",2002);
+        b.getHobby("Gaming");
+        b.getZodiac("Scorpio");
+        b.getFavFood("Padang Rice");
+        b.getDreams("Data Engineer");
+        
+    }
+}
